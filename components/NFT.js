318f7afa71e0f8cd164f5c28b6a93844bb9e8ffe
ellagoris <eduardofidera@gmail.com>
2022-02-28 01:32:45 -0300

    feat: use name

--- components/NFT.js
+++ components/NFT.js
@@ -9,12 +9,12 @@ export const NFT = (nft) => {
         className="text-left w-24
         cursor-pointer rounded-md shadow-xs
         mr-3 mb-3 sm:mr-4 hover:underline text-center"
-        href={`/${nft.id}`}
+        href={`/${nft.name}`}
       >
         <img src={nft.image} className="rounded-md h-auto bg-black" />
         <div className="rounded-b-md py-2 px-2">
           <h3 className="text-xs text-gray-200">
-            [#{nft.rarity_rank}] {nft.id}
+            [#{nft.rarity_rank}] {nft.name}
           </h3>
         </div>
       </a>
